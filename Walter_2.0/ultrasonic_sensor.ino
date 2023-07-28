b346dc1180c33e26cfb98c7abdc55e84d15d22bc
Garth <59071040+gmhull@users.noreply.github.com>
2023-07-28 10:55:28 -0400

    Update var names

--- Walter_2.0/ultrasonic_sensor.ino
+++ Walter_2.0/ultrasonic_sensor.ino
@@ -1,15 +1,14 @@
 void send_signal(sensor_num) {
-  digitalWrite(triggerPin[sensor_num], HIGH);
+  digitalWrite(trigPin[sensor_num], HIGH);
   delayMicroseconds(10);
-  digitalWrite(triggerPin[sensor_num], LOW);
+  digitalWrite(trigPin[sensor_num], LOW);
 
   echo_timer[sensor_num] = micros();
 }
 
 float receive_signal(sensor_num) {
-  float distance;
-  time = pulseIn(echoPin[sensor_num], HIGH);
-  distance = (0.034 * time) / 2;
+  float time = pulseIn(echoPin[sensor_num], HIGH);
+  float distance = (0.034 * time) / 2.;
   
   return distance;
 }
